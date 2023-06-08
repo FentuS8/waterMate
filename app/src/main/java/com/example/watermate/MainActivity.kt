02856e5f2c5cc95c0b82946a620f8b45a9a6a4f2
FentuS8 <gromov.n@edu.narfu.ru>
2023-06-08 13:48:37 +0300

    first screen + tile

--- app/src/main/java/com/example/watermate/MainActivity.kt
+++ app/src/main/java/com/example/watermate/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.watermate
 
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Удаление полосы сверху экрана
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 }
-// ghj
