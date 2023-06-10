a482454e1683eaa8aeac1dec99fd0f8a28d8ceda
FentuS8 <gromov.n@edu.narfu.ru>
2023-06-10 21:12:48 +0300

    welcome screen. problems w/buttom color

--- app/src/main/java/com/example/watermate/MainActivity.kt
+++ app/src/main/java/com/example/watermate/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.watermate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import android.os.Bundle
+import android.content.res.Resources
+import android.widget.ListView
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +13,18 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val abilitiesArray = resources.getStringArray(R.array.possibilities_list)
+
+        val listView = findViewById<ListView>(R.id.possibilities)
+
+        val adapter = CustomArrayAdapter(this, android.R.layout.simple_list_item_1, abilitiesArray)
+        listView.adapter = adapter
+
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // Удаление полосы сверху экрана
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
